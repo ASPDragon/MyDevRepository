cba0cd3e0e8554990a76fd15ce7dc26dd1177953
ASPDragon <aspdrago@gmail.com>
2021-02-14 14:11:50 +0800

    Lesson_5_HW/HomeWork.cpp has been replaced by Lesson_5_HW/HomeWork_1.cpp

---
+++ GeekBrains_HW/Lesson_5_HW/HomeWork_1.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+void swap(int &var1, int &var2){
+    int t = var1;
+    var1 = var2;
+    var2 = t;
+}
+
+int main(int argc, char *argv[])
+{
+    //1
+    int var1 = 1, var2 = 0, i;
+    int *p1 = &var1;
+    int *p2 = &var2;
+    int Arr[5] = { *p1, *p2, *p2, *p1, *p2};
+    swap(Arr[*p1], Arr[*p2]);
+
+    do {
+        std::cout << Arr[i] << " ";
+        i++;
+    }while(i <= 4);
+    return 0;
+}
