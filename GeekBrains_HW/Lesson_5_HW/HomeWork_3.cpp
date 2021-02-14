42d9fd8f1981fad4f0a36efd4d40cf0d751360e9
ASPDragon <aspdrago@gmail.com>
2021-02-14 21:03:34 +0800

    HomeWork_3.cpp has been modified

--- GeekBrains_HW/Lesson_5_HW/HomeWork_3.cpp
+++ GeekBrains_HW/Lesson_5_HW/HomeWork_3.cpp
@@ -1,23 +1,25 @@
 #include <iostream>
 
-int equals(int *a, int *b);
+int equals(int *a, int *b, int *arr);
 
 int main(int argc, char *argv[])
 {
     int Arr[7] = { 1, 4, 5, 8, 2, 6, 10};
     int a, b;
-    equals(&a, &b);
+    equals(&a, &b, Arr);
 }
 
-int equals(int *a, int *b){
-    int Arr[7];
-    *a = Arr[0] + Arr[1] + Arr[2] + Arr[3];
-    *b = Arr[4] + Arr[5] + Arr[6];
+int equals(int *a, int *b, int *arr){
+    using std::cout;
+    using std::endl;
+
+    *a = arr[0] + arr[1] + arr[2] + arr[3];
+    *b = arr[4] + arr[5] + arr[6];
     if((*a) == (*b)){
-        std::cout << "true";
+        cout << "true" << endl;
     }
     else {
-        std::cout << "false";
+        cout << "false" << endl;
     }
 
 }
