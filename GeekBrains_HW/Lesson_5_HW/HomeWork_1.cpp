ad503f26d85f1a4cf80d7949cbeb523a20dcf144
ASPDragon <aspdrago@gmail.com>
2021-02-14 18:56:25 +0800

    HomeWork_1.cpp has been modified, HomeWork_2.cpp has been added

--- GeekBrains_HW/Lesson_5_HW/HomeWork_1.cpp
+++ GeekBrains_HW/Lesson_5_HW/HomeWork_1.cpp
@@ -9,10 +9,8 @@ void swap(int &var1, int &var2){
 int main(int argc, char *argv[])
 {
     int var1 = 1, var2 = 0, i;
-    int *p1 = &var1;
-    int *p2 = &var2;
-    int Arr[5] = { *p1, *p2, *p2, *p1, *p2};
-    swap(Arr[*p1], Arr[*p2]);
+    int Arr[5] = { var1, var2, var2, var1, var2 };
+    swap(Arr[var1], Arr[var2]);
 
     do {
         std::cout << Arr[i] << " ";
