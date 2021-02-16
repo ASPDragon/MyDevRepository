f8b271784db9266a91b8df4f45f62d89d7a25fe9
ASPDragon <aspdrago@gmail.com>
2021-02-16 16:43:06 +0800

    HomeWork-4 has been modified

--- GeekBrains_HW/Lesson_5_HW/HomeWork-4.cpp
+++ GeekBrains_HW/Lesson_5_HW/HomeWork-4.cpp
@@ -6,17 +6,30 @@ int main(int argc, char *argv[])
 {
     int Arr[] = { 17, 5, 42, 28, 56 };
     shift(Arr, 5);
-    for(int i = 4; i >= 0; i-- ){
+    for(int i = 0; i < 5; i++ ){
         std::cout << Arr[i] << ' ';
     }
     return 0;
 }
 
 void shift(int arr[], int size) {
-    int temp = arr[size - 1];
-        for(int i = size -1; i >= 0; i--){
-            arr[i] = arr[i-1];
+    int n, dir;
+    std::cout << "Input n: ";
+    std::cin >> n >> dir;
+    while(n) {
+        if(dir == 1){ // shift right
+            int temp = arr[0];
+            for(int i = 0 ; i < size; i++){
+                arr[i] = arr[i + 1];
+            }
+            arr[size - 1] = temp;
+        } else if(dir == 0){ // shift left
+            int temp = arr[size - 1];
+            for(int i = size - 1; i > 0; i--){
+                arr[i] = arr[i - 1];
+            }
+            arr[0] = temp;
         }
-        arr[0] = temp;
+        n--;
+    }
 }
-
