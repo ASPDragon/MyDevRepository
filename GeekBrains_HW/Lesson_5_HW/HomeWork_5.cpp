d08968343cc8ffe35c60ad3b37b2ec0276884c32
ASPDragon <aspdrago@gmail.com>
2021-02-17 16:52:46 +0800

    HomeWork_5.cpp has been modified

--- GeekBrains_HW/Lesson_5_HW/HomeWork_5.cpp
+++ GeekBrains_HW/Lesson_5_HW/HomeWork_5.cpp
@@ -1,1 +1,28 @@
 #include <iostream>
+#include <cstdarg>
+
+void swap1(int *arr, int size, ...);
+
+int main(int argc, char **argv)
+{
+    int Arr[] = { 1, 0, 1, 0, 1, 1, 0, 1 };
+    swap1(Arr, 8);
+    for(int i = 0; i < 8; i++){
+        std::cout << Arr[i] << " ";
+    }
+
+    return 0;
+}
+
+void swap1(int *arr, int size, ...){
+    va_list n;
+    int i;
+    va_start(n, size);
+    for(i = 0; i < size; i++){
+        va_arg(n, int);
+        if(arr[i] == 1) {
+            arr[i] = 0;
+        } else arr[i] = 1;
+    }
+    va_end(n);
+}
